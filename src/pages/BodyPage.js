42537398f925ac37fac519525d70d73669d437ce
JMK <jjsteel8@naver.com>
2025-08-14 18:06:44 +0900

    first

--- src/pages/BodyPage.js
+++ src/pages/BodyPage.js
@@ -8,23 +8,24 @@ import AddBody from './body/AddBody';
 import EditImage from './body/add/EditImage';
 import WritePost from './body/add/WritePost';
 
-export default function BodyPage(){
+
+export default function BodyPage() {
 
 
 
-    return(
+    return (
         <div className="bodyContainer">
-                <div style={{display:"flex"}}>
-                    <SidebarPage/>
-                </div>
-            <div style={{flex: 1 }}>
+            <div style={{ display: "flex" }}>
+                <SidebarPage />
+            </div>
+            <div style={{ flex: 1 }}>
                 <Routes>
-                    <Route path='/home' element={<HomeBodyPage/>}/>
-                    <Route path='/add' element={<AddBody/>}/>
-                    <Route path="/user" element={ <LoginPage/> }/>
-                    <Route path="/regist" element={ <RegisterUserPage/> }/>
+                    <Route path='/home' element={<HomeBodyPage />} />
+                    <Route path='/add' element={<AddBody />} />
+                    <Route path="/user" element={<LoginPage />} />
+                    <Route path="/regist" element={<RegisterUserPage />} />
                     <Route path="/editImage" element={<EditImage />} />
-                <Route path="/writePost" element={<WritePost />} />
+                    <Route path="/writePost" element={<WritePost />} />
                 </Routes>
             </div>
         </div>
