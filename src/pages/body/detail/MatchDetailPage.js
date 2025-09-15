29ba721007ae0755ef865db9ab4c5d710f39c81b
JMK <jjsteel8@naver.com>
2025-09-15 09:49:39 +0900

    chat페이지 import 수정

--- src/pages/body/detail/MatchDetailPage.js
+++ src/pages/body/detail/MatchDetailPage.js
@@ -4,6 +4,7 @@ import { getMatchDetail } from "../../../api/match/Matches";
 import MatchCard from "../home/match/MatchCard";
 import { getCookie } from "../../../utils/Cookie";
 import "../../../styles/detail.css";
+import ChatWindow from "../chat/ChatWindow";
 import { getHomeAndAwayMatches ,getRecentMatchesByHome,getRecentMatchesByAway} from "../../../api/Detail";
 // 팀 로고 컴포넌트
 const TeamLogo = ({ teamName, logoUrl }) => {
